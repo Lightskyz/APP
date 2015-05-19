91c07cdf7187c736e814547145a398615b917774
Lightskyz <antoine.rakotozafy@isep.fr>
2015-05-19 10:27:03 +0200

    inscription + connexion

--- www/modele/modele.php
+++ www/modele/modele.php
@@ -7,7 +7,7 @@
 <?php
 try
 {
-    $bdd = new PDO('mysql:host=localhost:8889;dbname=pear;charset=utf8','root', 'root');
+    $bdd = new PDO('mysql:host=localhost:3306;dbname=pear;charset=utf8','root', 'root');
 }
 catch (Exception $e)
 {
