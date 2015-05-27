4d8cb372627f876c6328efcb85439ece7e13a884
MaximePAYRAUDEAU <mpayraudeau5@gmail.com>
2015-05-27 10:12:30 +0200

    modif

--- www/controleurs/ajout_prod.php
+++ www/controleurs/ajout_prod.php
@@ -26,39 +26,43 @@ function addproduct($user){
 				$req -> bindParam(':description' , $description );
 				$req->execute();
 
-				$dossier = 'C:\Users\utilisateur\Desktop\APP\www\assets\img\products\ ';
-				$fichier = basename($_FILES['avatar']['name']);
-				$taille_maxi = 100000;
-				$taille = filesize($_FILES['avatar']['tmp_name']);
-				$extensions = array('.png', '.gif', '.jpg', '.jpeg');
-				$extension = strrchr($_FILES['avatar']['name'], '.');//Début des vérifications de sécurité..
-				if(!in_array($extension, $extensions)) //Si l'extension n'est pas dans le tableau
-				{
-    				 $erreur = 'Vous devez uploader un fichier de type png, gif, jpg, jpeg, txt ou doc...';
-				}
-				if($taille>$taille_maxi)
-				{
-    				 $erreur = 'Le fichier est trop gros...';
-				}
-				if(!isset($erreur)) //S'il n'y a pas d'erreur, on upload
-				{
-    			 //On formate le nom du fichier ici...
-    				 $fichier = strtr($fichier, 
-        				  'ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ', 
-        				  'AAAAAACEEEEIIIIOOOOOUUUUYaaaaaaceeeeiiiioooooouuuuyy');
-     				$fichier = preg_replace('/([^.a-z0-9]+)/i', '-', $fichier);
-   					if(move_uploaded_file($_FILES['avatar']['tmp_name'], $dossier . $fichier)) //Si la fonction renvoie TRUE, c'est que ça a fonctionné...
-    				{
-    				    echo 'Upload effectué avec succès !';
-    				} else //Sinon (la fonction renvoie FALSE).
-     				{
-     				    echo 'Echec de l\'upload !';
-    				}
-				} else
-				{
-    				 echo $erreur;
-				}
+				ajout_image();
 				echo'<p>Votre produit a bien été ajouté</p>';
 			}
 		}
+
+function ajout_image();
+	$dossier = 'C:\Users\utilisateur\Desktop\APP\www\assets\img\products\ ';
+	$fichier = basename($_FILES['avatar']['name']);
+	$taille_maxi = 100000;
+	$taille = filesize($_FILES['avatar']['tmp_name']);
+	$extensions = array('.png', '.gif', '.jpg', '.jpeg');
+	$extension = strrchr($_FILES['avatar']['name'], '.');//Début des vérifications de sécurité..
+	if(!in_array($extension, $extensions)) //Si l'extension n'est pas dans le tableau
+	{
+    	 $erreur = 'Vous devez uploader un fichier de type png, gif, jpg, jpeg, txt ou doc...';
+	}
+	if($taille>$taille_maxi)
+	{
+    	 $erreur = 'Le fichier est trop gros...';
+	}
+	if(!isset($erreur)) //S'il n'y a pas d'erreur, on upload
+	{
+     //On formate le nom du fichier ici...
+    	 $fichier = strtr($fichier, 
+    		  'ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ', 
+    		  'AAAAAACEEEEIIIIOOOOOUUUUYaaaaaaceeeeiiiioooooouuuuyy');
+    	$fichier = preg_replace('/([^.a-z0-9]+)/i', '-', $fichier);
+   		if(move_uploaded_file($_FILES['avatar']['tmp_name'], $dossier . $fichier)) //Si la fonction renvoie TRUE, c'est que ça a fonctionné...
+    	{
+    	    echo 'Upload effectué avec succès !';
+    	} else //Sinon (la fonction renvoie FALSE).
+    	{
+    	    echo 'Echec de l\'upload !';
+    	}
+	} else
+	{
+    	 echo $erreur;
+	}
+}
 ?>
