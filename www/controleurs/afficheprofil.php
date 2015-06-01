1d5a63f9b0a961ad75ca3300c228f7dfc20a5cc8
MaximePAYRAUDEAU <mpayraudeau5@gmail.com>
2015-06-01 10:41:13 +0200

    Modif AfficheProfil

--- www/controleurs/afficheprofil.php
+++ www/controleurs/afficheprofil.php
@@ -6,7 +6,7 @@ function afficheprofil($user){
 	while( $donnees = $req -> fetch()){
 		echo $donnees['nom'];
 		echo $donnees['prenom'];
-		echo $donnnees['email'];
+		echo $donnees['email'];
 		echo $donnees['born'];
 		echo $donnees['adresse'];
 		echo $donnees['cdp'];
